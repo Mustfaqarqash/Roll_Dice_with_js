bd7447e9024b77ccc532fa1ebeac7bb58fb031a2
Mustfaqarqash <133997345+Mustfaqarqash@users.noreply.github.com>
2024-06-07 19:00:50 +0300

    roll dice website with java script end

---
+++ scipt.js
@@ -0,0 +1,28 @@
+
+
+
+let container = document.getElementById("container");
+let doneTxt=document.getElementById("done");
+let imageTag = document.getElementById("image");
+
+function numGenerator() {
+    var n = Math.floor((Math.random() * 6)+1);
+    imageTag.setAttribute("src",`image/dice-${n}.png`);    
+}
+
+function imgGenerator() {
+    doneTxt.innerText = "";
+    let sumDelay = 0;
+    for (var i = 0; i < 12; i++) {
+        setTimeout(() => { numGenerator() }, 300 * i);
+       
+        //seconde = 300 300 600 1800 
+    }
+    sumDelay=(i * 300);
+   setTimeout(()=> doneTxt.innerText="Roll again!!" ,sumDelay )
+    
+
+}
+container.addEventListener("click", () => {
+    imgGenerator();
+});
